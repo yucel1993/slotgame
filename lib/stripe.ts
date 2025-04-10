113a677e6f4319f3fbee1fc4235de6a3b76f67de
Burhan <alpagut1993@gmail.com>
2025-04-10 16:59:25 +0300

    test

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -5,7 +5,7 @@ if (!process.env.STRIPE_SECRET_KEY) {
 }
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: "2023-10-16",
+  apiVersion: "2025-03-31.basil",
 })
 
 export const CREDIT_PACKAGES = [
